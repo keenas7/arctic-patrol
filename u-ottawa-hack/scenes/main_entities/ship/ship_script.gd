7c1c6a076e09a08bfc99c85ff5357caf8e2ea590
keenas7 <keenas@gmail.com>
2026-01-17 18:25:49 -0500

    fixed merge errors

--- u-ottawa-hack/scenes/main_entities/ship/ship_script.gd
+++ u-ottawa-hack/scenes/main_entities/ship/ship_script.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name Ship
 
 @export var nav_point_debug : Node2D
+@export var ship_display_name : String = "Big Chud"
 @export var fuel_capacity: float = 100.0
 @export var fuel_consumption_rate : float = 0.001
 @export var current_fuel: float = 100.0
