ec3b0bce98167b72451956bc86641edd5b6d1314
BradyParisien <bradyparisien37@gmail.com>
2026-01-17 10:45:27 -0500

    new branch, new ship UI & stuff

--- u-ottawa-hack/UI/ship_port_ui.gd
+++ u-ottawa-hack/UI/ship_port_ui.gd
@@ -1,9 +1,27 @@
-extends Control
+extends ColorRect
+class_name UiPort
 
 var port_label : Label
-var vertical_container = VBoxContainer
+var vertical_container : VBoxContainer
+var current_active_port : Port
+var adding_to_route : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	port_label = get_node("Label")
 	vertical_container = get_node("ScrollContainer/VBoxContainer")
+
+func openShipMenu(current_active_port_ : Port, docked_ships : Array[Ship]) -> void:
+	current_active_port = current_active_port_
+	for i in vertical_container.get_children():
+		i.free()
+	
+	for i in range(0,len(docked_ships)):
+		var button : Button = Button.new()
+		button.text = str(i+1) + ": "+docked_ships[i].name
+	
+	visible = true
+
+func closeMenu() -> void:
+	visible = false
+	adding_to_route = false
