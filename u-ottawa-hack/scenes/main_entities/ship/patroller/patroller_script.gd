15917027453c25891c3f8ad7d06ba07898c82d24
keenas7 <keenas@gmail.com>
2026-01-17 18:53:30 -0500

    little patroller, child of big chud

--- u-ottawa-hack/scenes/main_entities/ship/patroller/patroller_script.gd
+++ u-ottawa-hack/scenes/main_entities/ship/patroller/patroller_script.gd
@@ -10,3 +10,21 @@ func onClick():
 	if begin_new_nav:
 		navigator.target_position = get_global_mouse_position()
 		begin_new_nav = false
+
+
+#patroller-SPECIFIC PROPERTIES
+var quick_click_active: bool = true
+
+
+func _ready():
+	# Call parent's _ready first
+	super._ready()  # ← Runs BaseShip's _ready()
+	
+	# Now add frigate-specific setup
+	ship_display_name = "little scout"
+	ship_type = "patroller"
+	fuel_capacity: = 1000.0
+	current_fuel = 1000.0
+	fuel_consumption_rate : float = 0.01
+	max_speed = 200.0
+	speed = max_speed
--- u-ottawa-hack/scenes/main_entities/ship/ship_script.gd
+++ u-ottawa-hack/scenes/main_entities/ship/ship_script.gd
@@ -3,9 +3,15 @@ class_name Ship
 
 @export var nav_point_debug : Node2D
 @export var ship_display_name : String = "Big Chud" #Monster Destroyer, Fyre Frigate 
+@export var ship_type: String = "Default"
 @export var fuel_capacity: float = 100.0
 @export var fuel_consumption_rate : float = 0.001
 @export var current_fuel: float = 100.0
+@export var max_speed: float = 200.0
+@export var patrol_radius: float = 500.0
+
+#@export var sprite_path: String = "res://sprites/ships/frigate.png"
+
 
 var navigator : NavigationAgent2D
 #var begin_new_nav : bool = false
