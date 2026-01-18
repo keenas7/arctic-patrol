f7601c85c98fa5852f1c4e2ce2d9a0f3b9a4b189
Benoit Panda <128241730+benoit-p56@users.noreply.github.com>
2026-01-18 01:35:03 -0500

    Waves!! (Again)

--- u-ottawa-hack/scenes/Events/Weather/Waves/waves.gd
+++ u-ottawa-hack/scenes/Events/Weather/Waves/waves.gd
@@ -1,32 +1,33 @@
-extends Node2D
+extends Area2D
+class_name Waves
 
-var waves = Area2D.new()
+var collision : CollisionShape2D
+var sprite : Sprite2D
+var timer: Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	waves.global_position = Vector2(randi() % 100, randi() % 100)
-	#waves.global_position = Vector2.ZERO
+	global_position = Vector2(randi() % 1000, randi() % 1000) #Spawns the wave randomly
+	set_deferred("Size", Vector2(randi() % 500, randi() % 500))
+	timer.start(15)
+	timer.timeout.connect(_generate_waves)
 	
+	
+func _generate_waves():
+	queue_free()
+	var new_wave: Waves = preload("res://scenes/Events/Weather/Waves/waves.tscn").instantiate()
+	new_wave.global_position = Vector2(randi() % 1000, randi() % 1000)
+	new_wave.set_deferred("Size", Vector2(randi() % 500, randi() % 500))
+	timer.start(15)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	await get_tree().create_timer(randi() % 30).timeout
-	create_new_instance()
+
+func _on_body_entered(_body: Node2D) -> void:
+	for i in get_overlapping_bodies():
+		if i is Ship:
+			i.position -= i.position * .5
+			i.fuel_consumption_rate *= 1.25
 
 
-func _on_body_entered(body: Node2D) -> void:
-	if body is Ship:
-		body.position -= body.position * .5
-		body.fuel_consumption_rate *= 1.25
-		
 func _on_body_exited(body: Node2D) -> void:
 	if body is Ship:
-		body.fuel_consumption_rate /= 2
-
-func create_new_instance():
-	var new_waves_sce: PackedScene = preload("res://scenes/Events/Weather/Waves/waves.tscn")
-	var new_waves = new_waves_sce.instantiate()
-	new_waves.global_position = Vector2(randi() % 100, randi() % 100)
-	add_child(new_waves)
-	
-	
+		body.fuel_consumption_rate /= 1.25
